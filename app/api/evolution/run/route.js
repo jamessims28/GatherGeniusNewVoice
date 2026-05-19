12c4795cd92460ece4e905de4870d83f0fe2a718
jamessims28 <jamessims28@gmail.com>
2026-05-19 16:22:10 -0400

    Delete app directory

--- app/api/evolution/run/route.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { runEvolutionStack } from "../../../../lib/evolution/evolutionOrchestrator";
-
-export async function POST(req) {
-  const body = await req.json();
-  const result = runEvolutionStack(body || {});
-  return NextResponse.json(result);
-}
