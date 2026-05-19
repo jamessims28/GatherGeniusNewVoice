9502c74191e7c52afb3b487716ae9469e252a6ef
jamessims28 <jamessims28@gmail.com>
2026-05-19 15:57:34 -0400

    Delete components directory

--- components/RealtimeGeniusGatherCore.jsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import RealtimeConversationCore from "./RealtimeConversationCore";
-
-export default function RealtimeGeniusGatherCore(props) {
-  return <RealtimeConversationCore {...props} />;
-}
